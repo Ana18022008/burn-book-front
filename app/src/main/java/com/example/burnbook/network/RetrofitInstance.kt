524163e604a760a30fd04024714b257b9ab77c35
Hugo <hugo_paim@estudante.sesisenai.org.br>
2026-03-30 11:19:15 -0300

    MIC-32 fix: corrige porta de consumo da api para o que foi configurado no CORS do backend

--- app/src/main/java/com/example/burnbook/network/RetrofitInstance.kt
+++ app/src/main/java/com/example/burnbook/network/RetrofitInstance.kt
@@ -8,7 +8,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
 
-    private const val BASE_URL = "http://10.0.2.2:8080/"
+    private const val BASE_URL = "http://10.0.2.2:8082/"
 
     fun create(tokenDataStore: TokenDataStore): Retrofit {
         val logging = HttpLoggingInterceptor().apply {
